a99526f85440a7d3414b10525d86b96bae3e21ea
esbradshaw2 <elizabethbradshaw2@gmail.com>
2024-02-25 17:43:19 -0800

    figured out net total profit/loss, turning values in a list into integers, and displaying a number as a dollar amount in the print statement

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,21 +19,35 @@ import csv
 #set the path to chosen csv file
 csv_file=os.path.join('Resources','budget_data.csv')
 
-#specifying column index to extract total number of months
+#specifying column indexes for date and profit/loss
 column_index_date = 0
+column_index_pl = 1
 
-#empty list to store column data
-column_list = []
+#empty lists to store column data
+column_list_date = []
+column_list_pl =[]
 
 #read csv file & extract data
 with open(csv_file, 'r') as file:
     csv_reader=csv.reader(file)
-    next(csv_reader)
-    for row in csv_reader:
-        column_list.append(row[column_index_date])
-        month_count=len(column_list)
 
-print("Total Months: ", month_count)
+    #skips header row
+    next(csv_reader)
+
+    for row in csv_reader:
+
+        #adds the values in the specified rows of the specified column into respective lists
+        column_list_date.append(row[column_index_date])
+        column_list_pl.append(row[column_index_pl])
+
+        #converting profit/loss column values into integers
+        pl_as_numbers=[int(value) for value in column_list_pl]
+        
+        #count number of items in list using len function
+        month_count=len(column_list_date)
+        net_total_pl=sum(pl_as_numbers)
+
+print("Total Months: ", month_count, (f"Total: ${net_total_pl}"))
 
 
 
