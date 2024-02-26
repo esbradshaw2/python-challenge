c49479d907fb3023d6044e903ad455d965b625b6
esbradshaw2 <elizabethbradshaw2@gmail.com>
2024-02-25 21:35:01 -0800

    cleaned up and debugged code, in final stages. smoothed out order of code and min/max value for changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,6 @@
 #                   -greatest increase in profits
 #                   -greatest decrease in profits
 
-
-#with open(file, 'r') as text:
-#    lines=text.read()
-#    print(lines)
-
 #lets us create file path across operating systems
 import os
 
@@ -27,52 +22,83 @@ column_index_pl = 1
 column_list_date = []
 column_list_pl =[]
 
-#read csv file & extract data
+#open csv file & read
 with open(csv_file, 'r') as file:
     csv_reader=csv.reader(file)
 
     #skips header row
     next(csv_reader)
 
+    #start to extract data
     for row in csv_reader:
 
         #adds the values in the specified rows of the specified column into respective lists
         column_list_date.append(row[column_index_date])
         column_list_pl.append(row[column_index_pl])
 
-        #converting profit/loss column values into integers
+        #convert profit/loss column values into integers so we can treat them as numbers with our functions
         pl_as_numbers=[int(value) for value in column_list_pl]
         
         #count number of items in list using len function
         month_count=len(column_list_date)
+        
         #add all profit/loss values together to get a net total
         net_total_pl=sum(pl_as_numbers)
         
         
-        #calculate changes in profit/loss from one date to the next
-        pl_changes = [] #empty list to store all change values
+
+        #empty list to store change values
+        pl_changes = []
+
+        #for each profit or loss value within the column...
         for pl_value in range(1, len(pl_as_numbers)):
+            
+            #calculate change from one row to the next
             calculated_changes = pl_as_numbers[pl_value] - pl_as_numbers[pl_value - 1]
+
+            #add these change values to the empty list
             pl_changes.append(calculated_changes)
+
+            #calculate average of all profit/loss changes over time, rounded to two decimal places
+            average_pl_change = round((sum(pl_changes) / len(pl_changes)), 2)
+
+            #find greatest increase and decrease in profit/loss by finding max and min value in profit/loss change list
+            greatest_increase = max(pl_changes)
+            greatest_decrease = min(pl_changes)
+
+            #create a dictionary to store the change values and dates together so they can be cross-referenced
             pl_change_dictionary = {
                                     'change': pl_changes, 
                                     'date': column_list_date
                                     }
-            date_index = pl_change_dictionary['change'].index(max(pl_changes))
-            corresponding_date = pl_change_dictionary['date'][date_index + 1]
+            
+            #find the greatest increase value within the change list...
+            date_index_1 = pl_change_dictionary['change'].index(max(pl_changes))
 
-            #calculate average of all pforit/loss changes over time
-            average_pl_change = round((sum(pl_changes) / len(pl_changes)), 2)
+            #...and find the corresponding date for the greatest increase value
+            #"plus 1" because a change doesn't actually happen corresponding with the first date
+            corresponding_date_increase = pl_change_dictionary['date'][date_index_1 + 1]
 
+            #find the greatest decrease value within change list...
+            date_index_2 = pl_change_dictionary['change'].index(min(pl_changes))
 
-            greatest_increase = max(pl_changes)
-            greatest_decrease = min(pl_changes)
+            #... and find the corresponding date for the greatest decrease value
+            corresponding_date_decrease = pl_change_dictionary['date'][date_index_2 + 1]
+
+        
 
      
 
 
-print("Total Months: ", month_count, (f"Total: ${net_total_pl}"), (f"Average Change: ${average_pl_change}"), (f"Greatest Increase in Profits:  (${greatest_increase})"), (f"Greatest Decrease in Profits: (${greatest_decrease})"))
-print(corresponding_date)
+print(
+    "Financial Analysis",
+    "--------------------------------",
+    "Total Months: ", month_count,
+    (f"Total: ${net_total_pl}"),
+    (f"Average Change: ${average_pl_change}"),
+    (f"Greatest Increase in Profits: {corresponding_date_increase} (${greatest_increase})"),
+    (f"Greatest Decrease in Profits: {corresponding_date_decrease} (${greatest_decrease})")
+    )
 
 
 
