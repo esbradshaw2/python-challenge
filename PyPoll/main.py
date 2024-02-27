860b3b676a82f4ea3527b4b74447572825efb50c
esbradshaw2 <elizabethbradshaw2@gmail.com>
2024-02-26 23:52:32 -0800

    got most of the way through, totally forgot there was a whole other portion to this assignment so if i get the chance i just need to find out how to pick the winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,15 +14,16 @@ import csv
 #set the path to chosen csv file
 csv_file=os.path.join('Resources','election_data.csv')
 
-#specifying column indexes for date and profit/loss
-column_index_voterid = 0
-column_index_county = 1
+#specifying column indexes
 column_index_candidate = 2
 
 #empty lists to store column data
-column_list_voterid = []
-column_list_county = []
 column_list_candidate = []
+
+candidate_dict = {}
+
+    # {name: count,
+    #  name: count}
 
 #open csv file & read
 with open(csv_file, 'r') as file:
@@ -33,3 +34,49 @@ with open(csv_file, 'r') as file:
 
     #start to extract data
     for row in csv_reader:
+
+        #adds the values in the specified rows of the specified column into respective lists
+        column_list_candidate.append(row[column_index_candidate])
+
+        #count number of voters in list using len function
+        vote_count=len(column_list_candidate)
+
+        #find all candidates who recieved votes, and the total number of their votes
+        name = row[2]
+        if name in candidate_dict:
+            candidate_dict[name] = candidate_dict[name] + 1
+        else:
+            candidate_dict[name]=1
+        
+
+#print results to terminal
+#using "\n" to physically separate each value into different rows
+print("Election Results", "--------------------------", f"Total Votes: {vote_count}", "--------------------------", sep='\n')
+
+for name, count in candidate_dict.items():
+    percentage = (count / vote_count) * 100
+    print(f'{name}: {percentage:.3f}% ({count})')
+
+#for name, count in candidate_dict.items():
+    #candidate_winner = max(percentage)
+
+print("--------------------------", "Winner: candidate_winner", "--------------------------", sep='\n')
+
+
+
+
+#set the file name for the text file
+file_name = "pypoll_results.txt"
+
+#export analysis as the text file
+with open(file_name, 'w') as file:
+    print("Election Results", "--------------------------", f"Total Votes: {vote_count}", "--------------------------", sep='\n', file=file)
+
+    for name, count in candidate_dict.items():
+        percentage = (count / vote_count) * 100
+        print(f'{name}: {percentage:.3f}% ({count})', file=file)
+
+    #for name, count in candidate_dict.items():
+        #candidate_winner = max(percentage)
+
+    print("--------------------------", "Winner: candidate_winner", "--------------------------", sep='\n', file=file)
